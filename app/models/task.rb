883d95d194a7f598a95fa8aeb22e04d0b3cd1e34
makiko <abcmakiko@gmail.com>
2020-09-11 15:42:45 +0900

    gemのインストール

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  deadline   :date
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Task < ApplicationRecord
   belongs_to :board
 end
