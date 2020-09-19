50e1fb1b6658de5b78c3d8313964bb6c8eeffb23
makiko <abcmakiko@gmail.com>
2020-09-19 18:22:30 +0900

    tasksとuserを紐づける

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,12 +9,15 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  board_id   :bigint
+#  user_id    :bigint
 #
 # Indexes
 #
 #  index_tasks_on_board_id  (board_id)
+#  index_tasks_on_user_id   (user_id)
 #
 class Task < ApplicationRecord
   belongs_to :board
+  belongs_to :user
   validates :title, :deadline, presence: true
 end
